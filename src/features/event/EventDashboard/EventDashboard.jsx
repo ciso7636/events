e07db71c40f67af3606137ea5c3c9f7f4644fd7c
Lukas Cisar <lukas@MacBook-Pro-uzivatela-Macbook.local>
2018-09-25 19:27:20 +0200

    ver 1

---
+++ src/features/event/EventDashboard/EventDashboard.jsx
@@ -0,0 +1,90 @@
+import React, { Component } from 'react';
+import {connect} from 'react-redux';
+import { Grid, Button } from 'semantic-ui-react';
+import cuid from 'cuid';
+import EventList from '../EventList/EventList';
+import EventForm from '../EventForm/EventForm';
+
+const mapState = (state) => ({
+  events:state.events
+})
+
+class EventDashboard extends Component {
+  state = {
+    isOpen: false,
+    selectedEvent: null
+  };
+
+  handleFormOpen = () => {
+    this.setState({
+      selectedEvent: null,
+      isOpen: true
+    });
+  };
+
+  handleCancel = () => {
+    this.setState({
+      isOpen: false
+    });
+  };
+
+  handleUpdateEvent = (updatedEvent) => {
+    this.setState({
+      events: this.state.events.map(event => {
+        if (event.id === updatedEvent.id) {
+          return Object.assign({}, updatedEvent)
+        } else {
+          return event
+        }
+      }),
+      isOpen: false,
+      selectedEvent: null
+    })
+  }
+
+  handleOpenEvent = (eventToOpen) => () => {
+    this.setState({
+      selectedEvent: eventToOpen,
+      isOpen: true
+    })
+  } 
+
+  handleCreateEvent = (newEvent) => {
+    newEvent.id = cuid();
+    newEvent.hostPhotoURL = '/assets/user.png';
+    const updatedEvents = [...this.state.events, newEvent];
+    this.setState({
+      events: updatedEvents,
+      isOpen: false
+    })
+  }
+
+  handleDeleteEvent = (eventId) => () => {
+    const updatedEvents = this.state.events.filter(e => e.id !== eventId);
+    this.setState({
+      events: updatedEvents
+    })
+  }
+
+  render() {
+    const {selectedEvent} = this.state;
+    const {events} = this.props;
+    return (
+      <Grid>
+        <Grid.Column width={10}>
+          <EventList deleteEvent={this.handleDeleteEvent} events={events} onEventOpen={this.handleOpenEvent} />
+        </Grid.Column>
+        <Grid.Column width={6}>
+          <Button
+            onClick={this.handleFormOpen}
+            positive
+            content="Create Event"
+          />
+          {this.state.isOpen && <EventForm updateEvent={this.handleUpdateEvent} selectedEvent={selectedEvent} handleCancel={this.handleCancel} createEvent={this.handleCreateEvent} />}
+        </Grid.Column>
+      </Grid>
+    );
+  }
+}
+
+export default connect(mapState)(EventDashboard);
---
+++ src/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import 'semantic-ui-css/semantic.min.css';
+import './index.css';
+import App from './app/layout/App';
+import registerServiceWorker from './registerServiceWorker';
+import { configureStore } from './app/store/configureStore';
+
+const store = configureStore();
+
+const rootEl = document.getElementById('root');
+
+// let render = () => {
+  ReactDOM.render(
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>,
+    rootEl
+  );
+//};
+
+// if (module.hot) {
+//   module.hot.accept('./app/layout/App', () => {
+//     setTimeout(render);
+//   });
+// }
+
+// render();
+
+registerServiceWorker();
