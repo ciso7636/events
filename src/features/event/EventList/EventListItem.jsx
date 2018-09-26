1062bbd2370804d11f338e9f4d28f4da3d1fec0a
Lukas Cisar <cisar@administrators-MacBook-Pro-2.local>
2018-09-26 16:55:28 +0200

    Leason 70

--- src/features/event/EventList/EventListItem.jsx
+++ src/features/event/EventList/EventListItem.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { Segment, Item, Icon, List, Button } from 'semantic-ui-react';
-import EventListAttendee from './EventListAttendee'
+import EventListAttendee from './EventListAttendee';
+import { Link } from 'react-router-dom';
 
 class EventListItem extends Component {
   render() {
@@ -37,7 +38,7 @@ class EventListItem extends Component {
         <Segment clearing>
         <span>{event.description}</span>
           <Button onClick={deleteEvent(event.id)} as="a" color="red" floated="right" content="Delete" />
-          <Button onClick={onEventOpen(event)} as="a" color="teal" floated="right" content="View" />
+          <Button as={Link} to={`/event/${event.id}`} color="teal" floated="right" content="View" />
         </Segment>
       </Segment.Group>
     );
